f503cfa0fa28b7a57ed2b11d25609d9d19ff2ccc
zdh1207git <gotoseeyou@qq.com>
2019-12-05 17:16:38 +0800

    initial commit

---
+++ client/src/components/AppNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Community</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarColor01"
+      aria-controls="navbarColor01"
+      aria-expanded="true"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</template>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community</h1>
+      <p class="lead">A fully open source form. Made with ♥ by Coding Gardan.</p>
+      <hr class="my-4" />
+      <p>This forum is all about community. Be kinkd! Be helpful!🌈</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home"
+};
+</script>
